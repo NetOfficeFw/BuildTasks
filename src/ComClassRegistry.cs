c1faaa513f9728c188e5f321cb38bf48c06b373f
Jozef Izso <jozef.izso@gmail.com>
2021-08-04 14:25:28 +0200

    Code cleanup

--- src/ComClassRegistry.cs
+++ src/ComClassRegistry.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.Build.Utilities;
-using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Build.Utilities;
+using Microsoft.Win32;
 
 namespace NetOffice.Build
 {
